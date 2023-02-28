7e83767f57ac146fb14436be6900a6e40d4876cc
lapnd <nguyenduclap23111999@gmail.com>
2023-02-28 21:55:03 +0700

    Phien ban master branch: print function

--- code.cpp
+++ code.cpp
@@ -10,6 +10,16 @@ int tim_max(int *a, int n)
 	// cau lenh ham tim max
 }
 
+int tim_min(int *a, int n)
+{
+	// cau lenh tim min
+}
+
+void print_value(int *a, int n)
+{
+	// ma lenh ham in gia tri
+}
+
 int main()
 {
 	cout << "Phien ban dau tien";
